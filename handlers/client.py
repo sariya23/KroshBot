ff6b5767f2a824798a53879082e5e2d8b75ab232
sariya <dayyyzwait@gmail.com>
2023-02-08 13:08:59 +0300

    Created start inline keyboard to show commands

--- handlers/client.py
+++ handlers/client.py
@@ -1,6 +1,14 @@
 from aiogram import types, Dispatcher
 from create_bot import bot, dp
+from keyboards.client_kb import client_keyboard_start
 
 
 async def empty(message: types.Message):
-    await bot.send_message(f'Я кролик-бот и реагирую только на определенные команды, прям как настоящий кролик')
+    """An empty handler does not work on commands"""
+    await bot.send_message(message.from_user.id,
+                           f'Я кролик-бот и реагирую только на определенные команды, прям как настоящий кролик',
+                           reply_markup=client_keyboard_start)
+
+
+def register_handlers_client(dp: Dispatcher):
+    dp.register_message_handler(empty)
--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -1,4 +1,6 @@
-from from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-start_button =
+button_show_commands = InlineKeyboardButton('Я ОБУЧЕН КОМАНДАМ(тыкни)', callback_data='start')
+client_keyboard_start = InlineKeyboardMarkup()
+client_keyboard_start.add(button_show_commands)
