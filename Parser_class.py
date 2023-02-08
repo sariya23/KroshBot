5b9f33cc52e77ac516e1bf77088cb26d795bc0ef
sariya <dayyyzwait@gmail.com>
2023-02-08 15:11:04 +0300

    Created parser class for parse info.

--- Parser_class.py
+++ Parser_class.py
@@ -6,15 +6,38 @@ import requests
 
 
 class Parser:
+    """
+    Class Parser use for parsing info about rabbits
+    General use - take info from div tag with class 'product-wrapper'
+
+    Note:
+        Web-site: https://tsarskiykrolik.com/
+
+    Methods:
+    --------------------
+    get_hours_from_td(data: datetime.timedelta) - Returns hours from timedelta
+
+    convert_hours_to_word(hour: int) - Changes the ending of a word for correct form
+
+    convert_days_to_word(day: int) - Changes the ending of a word for correct form
+
+    parse_date(str_date: str) - Converts str date to datetime type
+
+    format_datetime(date: datetime.timedelta) - Returns a string with the number of days and hours until the end of the discount
+
+    parse(breed: str) - Returns a list of namedtuple with information about rabbits
+    """
     def __init__(self):
         pass
 
     @staticmethod
     def get_hours_from_td(date: timedelta) -> int:
+        """Returns hours from timedelta"""
         return date.seconds // 3600
 
     @staticmethod
     def convert_hours_to_word(hour: int) -> str:
+        """Returns correct ending of a word(час/часа/часов)"""
         if hour <= 4:
             return 'часа'
         else:
@@ -22,6 +45,7 @@ class Parser:
 
     @staticmethod
     def convert_days_to_word(day: int) -> str:
+        """Returns correct ending of a word(день, дня, дней)"""
         if day == 1:
             return 'день'
         elif 2 <= day <= 4:
@@ -31,9 +55,12 @@ class Parser:
 
     @staticmethod
     def parse_date(str_date: str) -> datetime:
+        """Returns date at datetime type"""
         return datetime.strptime(str_date, '%Y-%m-%d  %H:%M:%S')
 
     def format_datetime(self, date: timedelta) -> str:
+        """Returns a string with the number of days and hours until the end of the discount.
+        If days until the end of discount less 0, only the hours are returned"""
         time_end = (date.days, self.get_hours_from_td(date))
 
         if time_end[0] == 0 and time_end[1] > 0:  # if days < 0
@@ -42,6 +69,9 @@ class Parser:
             return f'{time_end[0]} {self.convert_days_to_word(time_end[0])} и {time_end[1]} {self.convert_hours_to_word(time_end[1])}'
 
     def parse(self, breed: str) -> list[namedtuple]:
+        """Takes breed from user(inline-button) and parse info about it from site.
+        The Exception is raised if no discount in block"""
+
         breeds = {
             'belichij': 'belichij',
             'germelin': 'germelin',
@@ -60,7 +90,7 @@ class Parser:
         res = []
 
         for block in products:
-            name = block.find('h3').text.strip()
+            breed = block.find('h3').text.strip()
             price = block.find('span', class_='price').text.split()
             img_url = block.find('img').get('data-srcset').split(', ')[0][:-4]
             more_info = block.find('h3').find('a')['href']
@@ -84,8 +114,8 @@ class Parser:
 
             old_price = ''.join(price[:price.index('₽') + 1])
             discount_price = ''.join(price[price.index('₽') + 1:])
-            Rabbit = namedtuple('Rabit', 'name, img_url, old_price, discount_price, time_to_disc_end, more_info')
-            rabbit = Rabbit(name, img_url, old_price, discount_price, self.format_datetime(time_to_end), more_info)
+            Rabbit = namedtuple('Rabit', 'breed, img_url, old_price, discount_price, time_to_disc_end, more_info')
+            rabbit = Rabbit(breed, img_url, old_price, discount_price, self.format_datetime(time_to_end), more_info)
 
             res.append(rabbit)
 
@@ -94,4 +124,4 @@ class Parser:
 
 if __name__ == '__main__':
     a = Parser()
-    print(*a.parse('minor'), sep='\n')
+    print(*a.parse('germelin'), sep='\n')
