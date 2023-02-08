8e98873acd7c83b73d829614bbc5c13310a08374
sariya <dayyyzwait@gmail.com>
2023-02-08 14:08:41 +0300

    Add callback to show commands and created inline keyboard of commands

--- handlers/client.py
+++ handlers/client.py
@@ -1,14 +1,24 @@
 from aiogram import types, Dispatcher
 from create_bot import bot, dp
 from keyboards.client_kb import client_keyboard_start
+from keyboards.client_kb import client_keyboard_commands
+from aiogram.dispatcher.filters import Text
 
 
 async def empty(message: types.Message):
     """An empty handler does not work on commands"""
     await bot.send_message(message.from_user.id,
-                           f'Я кролик-бот и реагирую только на определенные команды, прям как настоящий кролик',
+                           f'Я кролик-бот. Меня зовут Крош. Я реагирую только на определенные команды, прям как настоящий кролик',
                            reply_markup=client_keyboard_start)
 
 
+async def show_commands(callback: types.CallbackQuery):
+    """Show inline keyboard command"""
+    await bot.send_message(callback.from_user.id, 'Вот что я умею', reply_markup=client_keyboard_commands)
+    await callback.answer()
+
+
 def register_handlers_client(dp: Dispatcher):
+    """The function registers handlers"""
     dp.register_message_handler(empty)
+    dp.register_callback_query_handler(show_commands, Text(startswith=('show_commands')))
--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -1,6 +1,14 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+# Start inline
+button_show_commands = InlineKeyboardButton('Я ОБУЧЕН КОМАНДАМ(тыкни)', callback_data='show_commands')
 
-button_show_commands = InlineKeyboardButton('Я ОБУЧЕН КОМАНДАМ(тыкни)', callback_data='start')
 client_keyboard_start = InlineKeyboardMarkup()
 client_keyboard_start.add(button_show_commands)
+
+# Commands
+button_location = InlineKeyboardButton('Локация остальных кроликов', callback_data='location')
+button_phone_number = InlineKeyboardButton('Номер телефона', callback_data='phone_number')
+
+client_keyboard_commands = InlineKeyboardMarkup(row_width=1)
+client_keyboard_commands.add(button_location, button_phone_number)
